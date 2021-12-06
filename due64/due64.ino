87b207ffc522d30d1a00f1b74c2a9f884e87b0cb
david parker <17parker@gmail.com>
2021-12-05 17:46:56 -0800

    UART rx interrupt triggers PWM output

--- due64/due64.ino
+++ due64/due64.ino
@@ -9,53 +9,58 @@
 
 #include "SAM3XDUE.h"
 
+const uint16_t one = 7;
+const uint16_t zro = 21;
+const uint16_t stop = 14;
+const uint16_t off = 0;
+const uint16_t b[2] = { zro, one };
 
-volatile uint8_t uart_data[5];				//volatile because might update controller data with interrupts
-volatile uint8_t* volatile uart_data_ptr;	//volatile volatile, volatile
-volatile uint8_t total_tx_count = 0;	//how many bytes to tx
+uint16_t status_response[25] = { b[0], b[0], b[0], b[0], b[0], b[1], b[0], b[1],	//0x05
+								 b[0], b[0], b[0], b[0], b[0], b[0], b[0], b[0],	//0x00
+								 b[0], b[0], b[0], b[0], b[0], b[0], b[0], b[0],	//0x00
+								 stop };
+uint16_t status_count = 25;
 
 
-uint8_t status_response[3] = { 237, 0x00, 0x00 };
-uint8_t status_counter = 3;
+/*
+controller joystick x / y is capable of values 0x80 - 0x7F (-128 to 127)
+In reality, it is only capable of about +/- 72 (one resource says -81 to 81)
+*/
 
-uint8_t pwm_dtys[5] = { 100, 200, 255, 100, 50 };
-
-uint16_t dtys[2] = { 1000, 20000 };
-
-void pwm_update_unlock() { REG_PWM_SCUC = 1; }
-
+volatile uint16_t controller_data[33];
 
 void setup() {
+	for (uint8_t i = 0; i < 32; i++) {
+		controller_data[i] = zro;
+	}
+	controller_data[0] = one;
+	controller_data[32] = stop;
 	//Pin setup
 	pio_disable_pullup(PIOA, UTXD | URXD);
 	pio_enable_output(PIOA, UTXD);
 	pio_disable_output(PIOA, URXD);
 	pio_set_periph_mode_A(PIOA, UTXD | URXD);
 	pio_disable_pio(PIOA, URXD | URXD);
-
-	//PWM setup - PDF page 991 goes over how to setup DMA w/ PWM
-	pio_disable_pullup(PIOB, PWML0 | PIN_20B);
-	pio_enable_output(PIOB, PWML0 | PIN_20B);
-	pio_disable_pio(PIOB, PWML0 | PIN_20B);
-	pio_set_periph_mode_B(PIOB, PWML0 | PIN_20B);
+	pio_disable_pullup(PIOB, PWML0);
+	pio_enable_output(PIOB, PWML0);
+	pio_disable_pio(PIOB, PWML0);
+	pio_set_periph_mode_B(PIOB, PWML0);
 	pmc_enable_periph_clk(ID_PWM);
-	
-	//REG_PWM_CLK = 1 | (1 << 16)|(0b1010 << 8) | 255;
-	REG_PWM_CMR0 |= 0b0101;
-	REG_PWM_SCM |= 1;
-	REG_PWM_CPRD0 = 50000;
-	REG_PWM_CDTY0 = 25000;
-	REG_PWM_ENA |= 1;
-
-	REG_PWM_IER1 |= 1;
-	NVIC_ClearPendingIRQ(PWM_IRQn);
-	NVIC_SetPriority(PWM_IRQn, 0);
-	NVIC_EnableIRQ(PWM_IRQn);
-	while (1) {}
-
-
-	//UART setup
 	pmc_enable_periph_clk(ID_UART);
+
+	REG_PWM_CLK = 3 | (1 << 9);
+	REG_PWM_CMR0 |= 0b1011;	//CLKA
+	REG_PWM_SCM |= 1 | (1 << 17);
+	REG_PWM_CPRD0 = 28;
+	REG_PWM_CDTY0 = off;
+	REG_PWM_TPR = (uint32_t)status_response;
+	REG_PWM_TCR = 25;
+	REG_PWM_PTCR = (1 << 8);
+	REG_PWM_IER2 |= (1 << 2);
+	NVIC_ClearPendingIRQ(PWM_IRQn);
+	NVIC_SetPriority(PWM_IRQn, 1);
+	NVIC_EnableIRQ(PWM_IRQn);
+	REG_PWM_ENA |= 1;
 	/*
 	Baud rate = MCK / (16 * CD)
 	N64 controller BAUD rate = 250kHz
@@ -63,31 +68,28 @@ void setup() {
 	*/
 	uart_set_clk_div(21);
 	uart_set_parity_ch_mode();
-	//Interrupts and PDC setup
-	REG_UART_TNCR = 0;	//tx next counter = 0
-	REG_UART_RNCR = 0;	//rx next counter = 0;
+	REG_UART_TNCR = 0;
+	REG_UART_RNCR = 0;
 	REG_UART_IDR = ~0;
 	REG_UART_IER = 1;	//enable rxrdy interrupt (data received has not been read yet)
 	NVIC_ClearPendingIRQ(UART_IRQn);
 	NVIC_SetPriority(UART_IRQn, 0);
 	NVIC_EnableIRQ(UART_IRQn);
-	uart_enable_rx_tx();
+	uart_enable_rx();
 }
 
-volatile uint8_t crnt_dty = 0;
+
 void PWM_Handler() {
-	if (crnt_dty == 0)
-		crnt_dty = 1;
-	else
-		crnt_dty = 0;
-	REG_PWM_CDTY0 = dtys[crnt_dty];
-	pwm_update_unlock();
+	volatile uint32_t dummy = REG_PWM_ISR2;
+	REG_PWM_CDTY0 = off;
+	//read controller data for next frame
 }
 
 void UART_Handler() {
 	switch (REG_UART_RHR) {
 	case 0x0:	//status - controller sends 3 bytes: 0x05, 0x00, 0x00 (bitflags)
-
+		REG_PWM_TPR = (uint32_t)status_response;
+		REG_PWM_TCR = 25;
 		break;
 
 	case 0xFE:	//poll - controller sends 4 bytes
