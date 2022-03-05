d2c567611cf05d0fdc024156dbab4896ae4d7754
david parker <17parker@gmail.com>
2022-03-04 20:57:07 -0800

    finally

--- due64/due64.ino
+++ due64/due64.ino
@@ -8,7 +8,6 @@ In reality, it is only capable of about +/- 72 (one resource says -81 to 81)
 #include "tas_data.h"
 #include "tasmc.h"
 
-
 const uint16_t status_response[28] = { off, off, b[0], b[0], b[0], b[0], b[0], b[1], b[0], b[1],	//0x05
 								 b[0], b[0], b[0], b[0], b[0], b[0], b[0], b[0],	//0x00
 								 b[0], b[0], b[0], b[0], b[0], b[0], b[0], b[0],	//0x00
